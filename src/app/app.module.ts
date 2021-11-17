2775bc6bbeef401091e53e85ca3bf97bc37eee52
Palloma Ruysa <pallomaruysa@gmail.com>
2021-11-17 15:25:53 -0300

    adicionado estilizacao nos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListarComponent } from './Model/Pessoa/listar/listar.component';
+import { EditarComponent } from './Model/Pessoa/editar/editar.component';
+import { AdicionarComponent } from './Model/Pessoa/adicionar/adicionar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListarComponent,
+    EditarComponent,
+    AdicionarComponent
   ],
   imports: [
     BrowserModule,
