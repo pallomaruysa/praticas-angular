f3b694a94468ce956bd2de5e48bc50732cf65df5
Palloma Ruysa <pallomaruysa@gmail.com>
2021-11-17 15:54:15 -0300

    adicionando rotas para listar e adicionar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { AdicionarComponent } from './Model/Pessoa/adicionar/adicionar.component';
+import { ListarComponent } from './Model/Pessoa/listar/listar.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'listar', component: ListarComponent},
+  {path: 'adicionar', component: AdicionarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
