57bb202addb934de9a83b70b0cbddbc7fdc060ed
Palloma Ruysa <pallomaruysa@gmail.com>
2021-11-18 13:52:09 -0300

    criando service(getPessoa) e entidade pessoa

---
+++ src/app/Model/Pessoa/entidade/Pessoa.ts
@@ -0,0 +1,5 @@
+export class Pessoa{
+    id?: Number;
+    nome?: String; // o ? é para ser opcional e não precisar do construtor
+    sobrenome?: String; // não é muito utilizado
+}
--- src/app/Service/servico.service.ts
+++ src/app/Service/servico.service.ts
@@ -7,4 +7,11 @@ import { HttpClient } from '@angular/common/http';
 export class ServicoService {
 
   constructor(private http: HttpClient) { }
+
+  
+  urlBase = "http://localhost:8080/pessoa";
+
+  getPessoa(){
+    return this.http.get<Pessoa[]>(this.urlBase)
+  }
 }
